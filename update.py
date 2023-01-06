2c845835a63820f2e5333ce556014051aebd245a
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2023-01-06 11:04:25 +0200

    Delete update.py

--- update.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def install():
-  os.system("git clone https://github.com/Rayan25062011/Beep/beep.js")
-  os.system("git clone https://github.com/Rayan25062011/Beep/index.html")
-  
-install()
