7aea834688176fbb90305b2df88fe2974051c36b
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2023-01-06 10:42:54 +0200

    Update install.py

--- install.py
+++ install.py
@@ -2,5 +2,6 @@ import os
 
 def install():
   os.system("git clone https://github.com/Rayan25062011/Beep/beep.js")
+  os.system("git clone https://github.com/Rayan25062011/Beep/index.html")
   
 install()
