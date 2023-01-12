f484dd8e3fc2176d481e0ba19cd4f30a875f95e0
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2023-01-12 20:01:21 +0200

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Test</h1>`;
